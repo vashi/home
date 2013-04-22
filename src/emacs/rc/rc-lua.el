6907b0656b6b1a4fa0d7f83fff9704af170de50a
Шлыков Василий <vash@mail50.stend>
2013-04-22 11:38:28 +0400

    Set our version.

---
+++ src/emacs/rc/rc-lua.el
@@ -0,0 +1,6 @@
+(add-to-list 'load-path "~/src/emacs/lua-mode/lua-mode.el")
+
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
